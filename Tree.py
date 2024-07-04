8b05ebdf877d16368b79a7cc04da8176ed28a534
zairulmazwan <zairulmazwan@gmail.com>
2024-07-04 13:08:29 +0100

    4 Jul 2024, 13.08

--- Tree.py
+++ Tree.py
@@ -116,32 +116,9 @@ class Binary_Tree:
         print(value, end=" ")
 
 
-data = [20,6,17,5,2,19,7,13]
-my_tree = Binary_Tree()
-
-my_tree.create_tree(data)
-
-print("Pre-Order")
-my_tree.travers_preorder(my_tree.root)
-
-print("\nPost-Order")
-my_tree.travers_postorder(my_tree.root)
-
-print("\nLevel-Order")
-my_tree.travers_levelorder(my_tree.root)
-
-print("\nContain node? ",my_tree.contain_node(5))
-
-print("\nFind smallest ",my_tree.find_smallest(my_tree.root))
-
-print("\nDelete a node ")
-my_tree.delete_recursion(my_tree.root,13)
-
-print("\nLevel-Order - After deleting a node")
-my_tree.travers_levelorder(my_tree.root)
 
 
-
+# ©Zairul Mazwan©
 
 
 
